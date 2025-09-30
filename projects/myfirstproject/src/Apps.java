03bddd5a5d269cf8391ce7eea2e7f7479c78b86c
victornjeru49 <victornjeru49@gmail.com>
2025-09-30 17:47:21 +0300

    objected orienated

--- projects/myfirstproject/src/Apps.java
+++ projects/myfirstproject/src/Apps.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class App {
+public class Apps {
     public static void main(String[] args) throws Exception {
 
         double width = 0;
---
+++ vehicle/vehicles/src/Car.java
@@ -0,0 +1,13 @@
+public class Car {
+    String make ="Ford";
+    String model ="Mustang";
+    int year = 2025;
+    double price = 58000.99;
+    boolean isRunning = false;
+
+    void start(){
+       isRunning = true;
+       System.out.println("You start the engine!");
+    }
+    
+}
